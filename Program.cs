348d63989172a34dd117f816214f5c11d4db359f
Anastasia <nastys11@gmail.com>
2022-09-03 19:47:34 +0300

    Добавление кода

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 namespace Semin2DZ2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+        Console.WriteLine("Введите число:");
+        int chislo = int.Parse(Console.ReadLine());
+        string NewChislo = Convert.ToString(chislo);
+        if (chislo / 10 > 10)
+          {
+             Console.WriteLine($"Третья цифра числа {chislo} это: {NewChislo[2]}");
+          }
+        else
+          {
+             Console.WriteLine($"Третьей цифры нет"); 
+          }
         }
     }
 }
