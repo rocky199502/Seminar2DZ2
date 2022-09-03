4f84286479eeaa87f7adff4492f2bf28041bb44f
Anastasia <nastys11@gmail.com>
2022-09-03 19:30:17 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semin2DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
